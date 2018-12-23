782db19ab20fdb4e8ba5100485cccb7bcc0c7d67
7orz <31398427+AchooLuv@users.noreply.github.com>
2018-12-23 21:38:23 +0800

    V-1.0

--- backToTheTop.js
+++ backToTheTop.js
@@ -4,47 +4,70 @@
 // @version      0.1
 // @description  Steam好友动态回到顶部(原生JavaScript)!
 // @author       AchooLuv
-// @match        https://steamcommunity.com/id/*/home/
+// @include      http*://steamcommunity.com/*
+// @include      http*://store.steampowered.com/*
 // @grant        none
 // ==/UserScript==
 
 (function _bTT() {
-        'use strict';
-        var eventObject = {
-            addOnload: function (fn) {
-                if (fn == null) {
-                    fn = document;
-                }
-                var oldOnload = window.onload;
-                if (window.onload != 'function') {
-                    window.onload = fn;
-                } else {
-                    window.onload = function () {
-                        oldOnload();
-                        fn();
-                    }
-                }
-            },
-            addHander: function (ele, type, hander) {
-                if (ele.addEventListerner) {
-                    ele.addEventListerner(type, hander, false);
-                } else if (ele.attachEvent) {
-                    ele.attachEvent('on' + type, hander);
-                } else {
-                    ele['on' + type] = hander;
+    'use strict';
+    var eventObject = {
+        addOnload: function (fn) {
+            if (fn == null) {
+                fn = document;
+            }
+            var oldOnload = window.onload;
+            if (window.onload != 'function') {
+                window.onload = fn;
+            } else {
+                window.onload = function () {
+                    oldOnload();
+                    fn();
                 }
             }
-        };
+        },
+        addHander: function (ele, type, hander) {
+            if (ele.addEventListerner) {
+                ele.addEventListerner(type, hander, false);
+            } else if (ele.attachEvent) {
+                ele.attachEvent('on' + type, hander);
+            } else {
+                ele['on' + type] = hander;
+            }
+        }
+    };
 
-        function toTop() {
-            var timer = null,
-                isTop = true,
-                clientHeight = document.documentElement.clientHeight || document.clientHeight,
-                targetTag = document.getElementById('global_header'),
-                _i = document.createElement('i'),
-                _top = document.createElement('a');
+    function toTop() {
+        var timer = null,
+            isTop = true,
+            clientHeight = document.documentElement.clientHeight || document.clientHeight,
+            targetTag = document.getElementById('global_header'),
+            _style = document.createElement('style'),
+            _i = document.createElement('i'),
+            _top = document.createElement('a');
+        _style.innerHTML = `
+        @font-face {
+            font-family: 'iconfont';  /* project id 979749 */
+            src: url('//at.alicdn.com/t/font_979749_6dqwoqp866g.eot');
+            src: url('//at.alicdn.com/t/font_979749_6dqwoqp866g.eot?#iefix') format('embedded-opentype'),
+            url('//at.alicdn.com/t/font_979749_6dqwoqp866g.woff') format('woff'),
+            url('//at.alicdn.com/t/font_979749_6dqwoqp866g.ttf') format('truetype'),
+            url('//at.alicdn.com/t/font_979749_6dqwoqp866g.svg#iconfont') format('svg');
+          }
+        .iconfont{
+        font-family:"iconfont" !important;
+        font-size:40px;font-style:normal;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-stroke-width: 0.2px;
+        -moz-osx-font-smoothing: grayscale;}`;
+        document.body.insertBefore(_style, document.body.firstElementChild);
+        _i.setAttribute('class', 'iconfont');
+        _i.style.cssText = 'display:block;width:100%;height:100%;color:white;';
+        _i.innerHTML = '&#xe6a8;';
+        _top.appendChild(_i);
+
         _top.setAttribute('href', 'javascript:;');
-        _top.style.cssText = 'position:fixed;bottom:8%;right:20%;display:none;width:40px;height:40px;background:#1f6083;border-radius:50%;overflow:hidden;z-index:5'; //响应改用百分比
+        _top.style.cssText = 'position:fixed;bottom:8%;right:20%;display:none;width:40px;height:40px;border-radius:50%;overflow:hidden;z-index:9999;'; //响应改用百分比
         targetTag.appendChild(_top);
         eventObject.addHander(window, 'scroll', function () {
             var _aTop = document.documentElement.scrollTop || document.body.scrollTop;
