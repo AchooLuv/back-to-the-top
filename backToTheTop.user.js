6cd8e1b72aa08648a820781e4baa109e68f239f5
7orz <31398427+AchooLuv@users.noreply.github.com>
2019-02-15 17:04:16 +0800

    V-1.1
    增加动画效果；更改图标

--- backToTheTop.user.js
+++ backToTheTop.user.js
@@ -2,7 +2,7 @@
 // @name         BackToTheTop
 // @namespace    https://github.com/AchooLuv/Back-To-The-Top
 // @icon         http://store.steampowered.com/favicon.ico
-// @version      1.0
+// @version      1.1
 // @description  Steam全站回到顶部脚本(原生JavaScript)!
 // @author       AchooLuv
 // @include      http*://steamcommunity.com/*
@@ -15,6 +15,20 @@
 (function _bTT() {
     'use strict';
     var eventObject = {
+        addOnload: function (fn) {
+            if (fn == null) {
+                fn = document;
+            }
+            var oldOnload = window.onload;
+            if (typeof window.onload != 'function') {
+                window.onload = fn;
+            } else {
+                window.onload = function () {
+                    oldOnload();
+                    fn();
+                };
+            }
+        },
         addHander: function (ele, type, hander) {
             if (ele.addEventListerner) {
                 ele.addEventListerner(type, hander, false);
@@ -33,15 +47,23 @@
             targetTag = document.getElementById('global_header'),
             _style = document.createElement('style'),
             _i = document.createElement('i'),
-            _top = document.createElement('a');
+            _top = document.createElement('a'),
+            head = document.head || document.getElementByTagName("head")[0] || document.documentElement,
+            link = document.createElement("link");
+
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("href", "https://cdn.bootcss.com/animate.css/3.7.0/animate.min.css");
+        link.setAttribute("charset", "utf-8");
+        head.appendChild(link);
         _style.innerHTML = `
         @font-face {
-            font-family: 'iconfont';  /* project id 979749 */
-            src: url('//at.alicdn.com/t/font_979749_6dqwoqp866g.eot');
-            src: url('//at.alicdn.com/t/font_979749_6dqwoqp866g.eot?#iefix') format('embedded-opentype'),
-            url('//at.alicdn.com/t/font_979749_6dqwoqp866g.woff') format('woff'),
-            url('//at.alicdn.com/t/font_979749_6dqwoqp866g.ttf') format('truetype'),
-            url('//at.alicdn.com/t/font_979749_6dqwoqp866g.svg#iconfont') format('svg');
+            font-family: 'iconfont';  /* project id 1046153 */
+            src: url('//at.alicdn.com/t/font_1046153_2dm8186tys1.eot');
+            src: url('//at.alicdn.com/t/font_1046153_2dm8186tys1.eot?#iefix') format('embedded-opentype'),
+            url('//at.alicdn.com/t/font_1046153_2dm8186tys1.woff2') format('woff2'),
+            url('//at.alicdn.com/t/font_1046153_2dm8186tys1.woff') format('woff'),
+            url('//at.alicdn.com/t/font_1046153_2dm8186tys1.ttf') format('truetype'),
+            url('//at.alicdn.com/t/font_1046153_2dm8186tys1.svg#iconfont') format('svg');
           }
         .iconfont{
         font-family:"iconfont" !important;
@@ -50,21 +72,30 @@
         -webkit-text-stroke-width: 0.2px;
         -moz-osx-font-smoothing: grayscale;}`;
         document.body.insertBefore(_style, document.body.firstElementChild);
-        _i.setAttribute('class', 'iconfont');
-        _i.style.cssText = 'display:block;width:100%;height:100%;color:white;';
-        _i.innerHTML = '&#xe6a8;';
+        _i.classList.add("iconfont");
+        _i.style.cssText = 'display:block;width:100%;height:100%;color:#e5f4fc;';
+        _i.innerHTML = '&#xe602;';
         _top.appendChild(_i);
+        _top.classList.add("animated");
         _top.setAttribute('href', 'javascript:;');
-        _top.style.cssText = 'position:fixed;bottom:8%;right:20%;display:none;width:40px;height:40px;border-radius:50%;overflow:hidden;z-index:9999;'; //响应改用百分比
+        _top.style.cssText = 'position:fixed;bottom:8%;right:20%;display:none;width:50px;height:50px;overflow:hidden;z-index:9999;';
         targetTag.appendChild(_top);
+
         eventObject.addHander(window, 'scroll', function () {
             var _aTop = document.documentElement.scrollTop || document.body.scrollTop;
-            _aTop >= clientHeight ? _top.style.display = 'block' : _top.style.display = 'none';
+            if (_aTop >= clientHeight) {
+                _top.style.display = 'block'
+                _top.classList.add('zoomIn')
+            } else {
+                _top.classList.remove('zoomIn')
+                _top.classList.add('fadeOutUp')
+            }
             if (!isTop) {
                 clearInterval(timer);
             }
             isTop = false;
         });
+
         eventObject.addHander(_top, 'click', function () {
             timer = setInterval(function () {
                 var _aTop = document.documentElement.scrollTop || document.body.scrollTop,
@@ -77,5 +108,5 @@
             });
         }, 50);
     }
-    toTop();
+    eventObject.addOnload(toTop);
 })();
